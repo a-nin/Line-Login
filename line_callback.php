e9a825565b223069a2081483f06f134564b14744
LAPTOP-8DJ1QH41\achak <kogeusagi12@gmail.com>
2017-08-06 23:54:44 +0900

    callback

---
+++ line_callback.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml-transitional.dtd'>
+<html xmlns='http://www.w3.org/1999/xhtml' lang='ja' xml:lang='ja'>
+<head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+<meta name='viewport' content='width=device-width,initial-scale=1.0,user-scalable=no' />
+<link rel=stylesheet type="text/css" href="style.css">
+</head>
+<body>
+<div class='all'>
+<div class='main'>
+  <?php
+
+
+
+   ?>
